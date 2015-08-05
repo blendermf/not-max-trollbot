5959e0314b19f2c30cb6da4ba4bce2db4441b0c0
Matthew Fraser <blendermf@gmail.com>
2015-08-05 04:24:39 -0400

    Updated the requirements to actually pull in the correct dev version of praw (for bottom sticky support). Updated schedule (I should probably move this functionality outside of this script so I don’t commit the schedule always, perhaps with a separate update schedule script that will take paramaters) Added protections against disconnects (as the script was constantly disconnecting from mysql way before it was needed), and made sure it works in a multiprocess environment)

--- notmaxtrollbot.py
+++ notmaxtrollbot.py
@@ -9,6 +9,10 @@ from apscheduler.executors.pool import ThreadPoolExecutor
 from datetime import datetime
 from pytz import timezone
 from dateutil.relativedelta import *
+from sqlalchemy import event
+from sqlalchemy import exc
+from sqlalchemy.engine import Engine
+import os
 
 import config
 
@@ -17,6 +21,21 @@ handler = MultiprocessHandler()
 r = praw.Reddit(user_agent="DiamondClub subreddit moderation script by /u/blendermf v 0.1.", handler=handler)
 
 o = OAuth2Util.OAuth2Util(r)
+
+@event.listens_for(Engine, "connect")
+def connect(dbapi_connection, connection_record):
+	connection_record.info['pid'] = os.getpid()
+
+@event.listens_for(Engine, "checkout")
+def checkout(dbapi_connection, connection_record, connection_proxy):
+	pid = os.getpid()
+	if 'pid' not in connection_record.info or connection_record.info['pid'] != pid:
+		connection_record.connection = connection_proxy.connection = None
+		raise exc.DisconnectionError(
+				"Connection record belongs to pid %s, "
+				"attempting to check out in pid %s" %
+				(connection_record.info['pid'], pid)
+		)
 
 scheduler = BackgroundScheduler()
 scheduler.add_jobstore('sqlalchemy', url='mysql+pymysql://{}:{}@{}/{}'.format(config.db_user, config.db_pass, config.db_host, config.db_name))
@@ -37,7 +56,7 @@ def post_diamondtime():
 
 	print('Diamond time posted.')
 
-scheduler.add_job(post_diamondtime, 'cron', day_of_week='wed', hour=3, minute=10, timezone=timezone('US/Eastern'), replace_existing=True, id="post_diamondtime")
+scheduler.add_job(post_diamondtime, 'cron', day_of_week='wed', hour=3, minute=10, start_date="2015-07-31", timezone=timezone('US/Eastern'), replace_existing=True, id="post_diamondtime")
 scheduler.start()
 scheduler.print_jobs()
 
